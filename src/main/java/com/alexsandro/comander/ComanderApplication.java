056f44ebbec8249b311e454506136bd420f5f6f7
Alexsandro Guedes <alexsandrodesenvolvedor@gmail.com>
2025-11-11 21:08:40 -0300

    Initial commit

---
+++ src/main/java/com/alexsandro/comander/ComanderApplication.java
@@ -0,0 +1,13 @@
+package com.alexsandro.comander;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComanderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComanderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexsandro/comander/ComanderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexsandro.comander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComanderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
